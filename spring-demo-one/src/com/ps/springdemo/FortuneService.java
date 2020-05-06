f04cce1d9db29b4aba22a3b7f0ee4da1a05df434
PS-07 <singh.puneet1997@gmail.com>
2020-05-07 05:22:37 +0530

    IoC and DI

---
+++ spring-demo-one/src/com/ps/springdemo/FortuneService.java
@@ -0,0 +1,5 @@
+package com.ps.springdemo;
+
+public interface FortuneService {
+	public String getFortune();
+}
--- spring-demo-one/src/com/ps/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/ps/springdemo/TrackCoach.java
@@ -2,9 +2,24 @@ package com.ps.springdemo;
 
 public class TrackCoach implements Coach {
 
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {}
+	
+	// define a constructor for dependency injection
+	public TrackCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 10km";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just do it: " + fortuneService.getFortune();
 	}
 
 }
